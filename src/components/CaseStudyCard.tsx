8d37613fc1a0c50c0a834d76f37d6363b7a405c3
Mustpha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2026-09-17 17:05:39 +0100

    feat (CaseStudyCard): Generalised the component to handle both home and work route

--- src/components/CaseStudyCard.tsx
+++ src/components/CaseStudyCard.tsx
@@ -1,28 +1,54 @@
+import { cn } from "cn";
 import { FaArrowRight } from "react-icons/fa";
+import type { CaseStudy } from "@/lib/data";
 
-export default function CaseStudyCard({ className }: { className?: string }) {
-	return (
-		<a href="/" className="contents">
-			<div
-				className={`${className} font-semibold border rounded-sm border-grey`}
-			>
-				<img
-					src="https://picsum.photos/seed/mountain/400/400"
-					className="w-full aspect-video"
-					alt=""
-				/>
-				<div className="flex items-center justify-between p-3">
-					<div>
-						<p className="capitalize text-xl">Kick Bot</p>
-						<p className="text-xs max-[320px]:text-xs text-muted-foreground">
-							SportsTech Mobile App
-						</p>
-					</div>
-					<p className="text-blue max-[320px]:text-xs text-sm flex items-center w-fit">
-						View Our Case Study <FaArrowRight />
-					</p>
-				</div>
-			</div>
-		</a>
-	);
+type CaseStudyProps = Omit<CaseStudy, "category"> & {
+  category?: string;
+  index?: number;
+  className?: string;
+};
+export default function CaseStudyCard({
+  className,
+  title,
+  description,
+  category,
+  image,
+  index,
+}: CaseStudyProps) {
+  const isSecondVariant = Boolean(category);
+  return (
+    <a href="/" className="contents">
+      <div className={`${className} font-semibold  rounded-sm`}>
+        <img src={image} className="w-full aspect-video" alt="" />
+        <div
+          className={cn(
+            "flex items-center gap-4 justify-between p-3",
+            category && "flex-col items-start",
+          )}
+        >
+          <div>
+            <div className="flex items-center gap-x-2 relative">
+              <p className={cn("capitalize text-2xl w-fit", index === 0 && "md:text-4xl")}>
+                {title}
+              </p>
+              {isSecondVariant && (
+                <span className=" -right-4  text-xs bg-grey text-muted-foreground p-0.5">
+                  {category}
+                </span>
+              )}
+            </div>
+            <p className="text-xs max-[320px]:text-xs text-muted-foreground">{description}</p>
+          </div>
+          <p
+            className={cn(
+              "text-blue max-[320px]:text-xs text-sm flex gap-x-1 items-center w-fit",
+              category && "text-black",
+            )}
+          >
+            View Our Case Study <FaArrowRight />
+          </p>
+        </div>
+      </div>
+    </a>
+  );
 }
